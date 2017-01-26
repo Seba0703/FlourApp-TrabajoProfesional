a6ce6b3e470bdd1770374522ae33412a7c7fde66
urodriguez <ucielrodriguez1990@gmail.com>
2017-01-26 03:01:42 -0300

    Agrego logica para los permisos en ABM usuarios

--- public/app/app_usuarios/agregadorUsuarioComponent.ts
+++ public/app/app_usuarios/agregadorUsuarioComponent.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
 import { UsuarioServices } from './usuarioServices';
 
 @Component({
@@ -7,40 +9,96 @@ import { UsuarioServices } from './usuarioServices';
 })
 
 export class AgregadorUsuarioComponent {
+  private usuarios: Response;
+
   private nombre: string;
   private contrasenia: string;
-  private permisos: string;
+  private permisos: string = "lectura";
 
   private mostrarModalAgregar: boolean = true;
 
-  constructor(private ptService: UsuarioServices){}
+  constructor(private uService: UsuarioServices){}
+
+  ngOnInit() {
+    console.log("ON INIT");
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios(){
+    console.log("CARGANDO USUARIOS");
+    // en el momento del subscribe es cuando se dispara la llamada
+    this.uService.getUsuarios()
+              .subscribe(
+                (usuarios) => {
+                  this.usuarios = usuarios;
+                  console.log(this.usuarios);
+                },
+                err => console.error("EL ERROR FUE: ", err)
+              );
+  }
+
+  onSelectionChange(permisosSelecionado: string){
+  	this.permisos = permisosSelecionado;
+  }
 
   agregar() {
-
     if(this.nombre){
+      if(this.nombreSinUsar(this.nombre)) {
+	      let usuario = {
+	          nombre:      this.nombre,
+	          contrasenia: this.contrasenia,
+	          permisos:    this.permisos,
+	      }
+	      
+	      console.log(usuario);
 
-      let usuario = {
-          nombre:      this.nombre,
-          contrasenia:               this.contrasenia,
-          permisos:    this.permisos,
+	      this.uService.agregarUsuario(usuario)
+	                    .subscribe(data => {
+	                        console.log("usuario creado!!!");
+	                        console.log(data);
+	                        this.mostrarModalAgregar = false;
+	                        alert("\t\t\t\t¡Usuario agregado!\n\nPulse 'Aceptar' para actualizar y visualizar los cambios");
+	                        window.location.reload();
+	                    }, error => {
+	                        console.log(JSON.stringify(error.json()));
+	                        alert("\t\t\t\t¡ERROR al agregar Usuario!\n\nRevise los campos");
+	                    });;
+      } else {
+      	alert("\t\t\t¡ERROR!\n\nYa existe un usuario con ese nombre");
       }
-      
-      console.log(usuario);
-
-      this.ptService.agregarUsuario(usuario)
-                    .subscribe(data => {
-                        console.log("usuario creado!!!");
-                        console.log(data);
-                        this.mostrarModalAgregar = false;
-                        alert("\t\t\t\t¡Usuario agregado!\n\nPulse 'Aceptar' para actualizar y visualizar los cambios");
-                        window.location.reload();
-                    }, error => {
-                        console.log(JSON.stringify(error.json()));
-                        alert("\t\t\t\t¡ERROR al agregar Usuario!\n\nRevise los campos");
-                    });;
     } else {
-      alert("\t\t\t\t¡ERROR!\n\nDebe proporcionar al menos un nombre");
+      alert("\t\t\t¡ERROR!\n\nDebe proporcionar al menos un nombre");
     }
+  }
+
+  private nombreSinUsar(nombre: string): boolean{
+  	/*IMPORTANTE: lo que hace el codigo es medio raro, pero lo hice asi porque no se puede tratar 
+  	al Response como un Array porque el coompilador da error, aunque si lo ejecutas no da error
+  	en tiempo de ejecucion*/
+
+  	let users:any = [];
+
+  	console.log("u= " + this.usuarios.toString().split(","));
+
+	var cantUsuarios = this.usuarios.toString().split(",").length;
+
+	console.log("CANT= " + cantUsuarios);
+
+	if(cantUsuarios != 1) {
+	    for (var i = 0; i < cantUsuarios; i++) {
+	    	users.push(this.usuarios[i]);
+	    }
+
+		for (let usuario of users) {
+			//console.log(usuario);
+			if(nombre == usuario.nombre) {
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return true;
+	}
   }
 
 }
--- public/app/app_usuarios/usuarioComponent.ts
+++ public/app/app_usuarios/usuarioComponent.ts
@@ -20,7 +20,7 @@ export class UsuarioComponent {
 
   private mostrarModalModificar: boolean = true;
   
-  constructor(private cService: UsuarioServices){
+  constructor(private uService: UsuarioServices){
     let dataLogin = JSON.parse(sessionStorage.getItem("dataLogin"));
     
     this.accionesEjecutables = dataLogin.permisos;
@@ -34,7 +34,7 @@ export class UsuarioComponent {
   cargarProductosTerminados(){
     console.log("CARGANDO CLIENTES");
     // en el momento del subscribe es cuando se dispara la llamada
-    this.cService.getUsuarios()
+    this.uService.getUsuarios()
               .subscribe(
                 (usuariosData) => {
                   this.usuarios = usuariosData;
@@ -49,7 +49,7 @@ export class UsuarioComponent {
     if (r == true) {
         console.log("You pressed OK!");
         console.log("ID borrado= " + id);
-        this.cService.borrarUsuario(id)
+        this.uService.borrarUsuario(id)
                       .subscribe(
                         () => { 
                       alert("\t\t\t\t¡Se borro existosamente!\n\nPulse 'Aceptar' para actualizar y visualizar los cambios");
@@ -63,36 +63,75 @@ export class UsuarioComponent {
   }
 
   modificar(usuario: any){
-    this._id =                usuario._id;
+    this._id =         usuario._id;
     this.nombre =      usuario.nombre;
-    this.contrasenia =               usuario.contrasenia;
+    this.contrasenia = usuario.contrasenia;
     this.permisos =    usuario.permisos;
+  }
+
+  onSelectionChange(permisosSelecionado: string){
+    this.permisos = permisosSelecionado;
   }
 
   guardarModificaciones(){
     if(this.nombre){
-      this.mostrarModalModificar = false;
-      let usuario = {
-          _id:                this._id,
-          nombre:      this.nombre,
-          contrasenia:               this.contrasenia,
-          permisos:    this.permisos
-      }
-      
-      console.log(usuario);
+      if(this.nombreSinUsar(this.nombre)) {
+        this.mostrarModalModificar = false;
+        let usuario = {
+            _id:         this._id,
+            nombre:      this.nombre,
+            contrasenia: this.contrasenia,
+            permisos:    this.permisos
+        }
+        
+        console.log(usuario);
 
-      this.cService.modificar(usuario)
-                    .subscribe(data => {
-                        console.log(data);
-                        
-                        alert("\t\t\t\t¡Usuario modificado!\n\nPulse 'Aceptar' para actualizar y visualizar los cambios");
-                        window.location.reload();                        
-                    }, error => {
-                        console.log(JSON.stringify(error.json()));
-                        alert("\t\t\t\t¡ERROR al modificar Usuario!\n\nRevise los campos");
-                    });;
+        this.uService.modificar(usuario)
+                      .subscribe(data => {
+                          console.log(data);
+                          
+                          alert("\t\t\t\t¡Usuario modificado!\n\nPulse 'Aceptar' para actualizar y visualizar los cambios");
+                          window.location.reload();                        
+                      }, error => {
+                          console.log(JSON.stringify(error.json()));
+                          alert("\t\t\t\t¡ERROR al modificar Usuario!\n\nRevise los campos");
+                      });;
+      } else {
+        alert("\t\t\t¡ERROR!\n\nYa existe un usuario con ese nombre");
+      }
+
     } else {
       alert("\t\t\t\t¡ERROR!\n\nDebe proporcionar al menos un nombre");
+    }
+  }
+
+  private nombreSinUsar(nombre: string): boolean{
+    /*IMPORTANTE: lo que hace el codigo es medio raro, pero lo hice asi porque no se puede tratar 
+    al Response como un Array porque el coompilador da error, aunque si lo ejecutas no da error
+    en tiempo de ejecucion*/
+
+    let users:any = [];
+
+    console.log("u= " + this.usuarios.toString().split(","));
+
+    var cantUsuarios = this.usuarios.toString().split(",").length;
+
+    console.log("CANT= " + cantUsuarios);
+
+    if(cantUsuarios != 1) {
+      for (var i = 0; i < cantUsuarios; i++) {
+        users.push(this.usuarios[i]);
+      }
+
+      for (let usuario of users) {
+      //console.log(usuario);
+        if(nombre == usuario.nombre) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      return true;
     }
   }
 
